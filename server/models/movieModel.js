8e924c5bce5cd0edfb6b1fdcc8b6337312b0bfc7
pat-doyle <pdoyle757@gmail.com>
2018-10-21 14:40:27 -0400

    skeleton for DB models created

--- server/models/movieModel.js
+++ server/models/movieModel.js
@@ -2,8 +2,38 @@
 //For SQL
 const sqlDb = require('../../db/sql');
 //For Mongo
-const mongoDb = require('../../db/mongodb')
+// const mongoDb = require('../../db/mongodb')
 
 module.exports = {
-
+  favorites: {
+    get: function(callback){
+      sqlDb.query('SELECT * FROM favorites', function(err, results){
+        if(err){
+          console.log('you fucked up');
+        } else {
+          callback(results);
+        }
+      })
+    },
+    post: function(params, callback){
+      params = 'whatever the fuck was clicked AKA ? below';
+      sqlDb.query('INSERT INTO favorites VALUE ?', params, function(err, results){
+        if(err){
+          console.log('you dumb');
+        } else {
+          callback(results);
+        }
+      })
+    },
+    delete: function(params, callback){
+      params = 'ID of whatever movie was selected';
+      sqlDb.query('DELETE FROM favorites WHERE id = ? ', params, function(err, results = '?'){
+        if(err){
+          console.log(err);
+        } else {
+          callback(results);
+        }
+      })
+    }
+  }
 }
